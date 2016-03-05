66663f230b280f3d6e7988565f77f5114a068e18
codingCookie <github@menat.de>
2016-03-05 14:58:49 +0100

    add 'join channel'

--- shared/actions/channels.js
+++ shared/actions/channels.js
@@ -2,26 +2,56 @@ import { SENDBIRD_LIST_CHANNELS, SENDBIRD_LIST_CHANNELS_ERROR } from '../constan
 import sendbird from 'sendbird'
 
 export function listChannels() {
-  const page = 1
-  const limit = 20
+    const page = 1
+    const limit = 20
 
-  return dispatch => {
-    sendbird.getChannelList({
-      page: page,
-      limit: limit,
-      successFunc: (data) => {
-        dispatch({
-          type: SENDBIRD_LIST_CHANNELS,
-          data: data
+    return dispatch => {
+        sendbird.getChannelList({
+            page: page,
+            limit: limit,
+            successFunc: (data) => {
+                dispatch({
+                    type: SENDBIRD_LIST_CHANNELS,
+                    data: data
+                })
+            },
+            errorFunc: (status, error) => {
+                dispatch({
+                    type: SENDBIRD_LIST_CHANNELS_ERROR,
+                    status: status,
+                    error: error
+                })
+            }
         })
-      },
-      errorFunc: (status, error) => {
-        dispatch({
-          type: SENDBIRD_LIST_CHANNELS_ERROR,
-          status: status,
-          error: error
-        })
-      }
-    })
-  }
+    }
+}
+
+
+export function joinChannel(url) {
+
+    return dispatch => {
+        sendbird.joinChannel(
+            url,
+            {
+                "successFunc": function (data) {
+                    console.log(data);
+                    sendbird.connect({
+                        "successFunc": function (data) {
+                            console.log(data);
+                            // do something
+                        },
+                        "errorFunc": function (status, error) {
+                            console.log(status, error);
+                            // do something
+                        }
+                    });
+                    // do something
+                },
+                "errorFunc": function (status, error) {
+                    console.log(status, error);
+                    // do something
+                }
+            }
+        );
+    }
 }
--- shared/components/pages/ChannelsPage.js
+++ shared/components/pages/ChannelsPage.js
@@ -1,14 +1,15 @@
 import React, {
-  Platform,
-  Component,
-  View,
-  ScrollView,
-  InteractionManager,
-  Switch
+    Platform,
+    Component,
+    View,
+    ScrollView,
+    InteractionManager,
+    TouchableHighlight,
+    Switch
 } from 'react-native'
 import { connect } from 'react-redux/native'
 
-import { listChannels } from '../../actions/channels'
+import { listChannels, joinChannel } from '../../actions/channels'
 
 import ActionBar from '../container/ActionBar'
 import LoadingIndicator from '../elements/LoadingIndicator'
@@ -16,60 +17,70 @@ import Text from '../elements/Text'
 import IntroText from '../elements/IntroText'
 
 class ChannelsPage extends Component {
-  state = {
-    interactionsFinished: false
-  };
+    state = {
+        interactionsFinished: false
+    };
 
-  componentDidMount() {
-    InteractionManager.runAfterInteractions(() => {
-      this.setState({interactionsFinished: true})
+    componentDidMount() {
+        InteractionManager.runAfterInteractions(() => {
+            this.setState({interactionsFinished: true})
 
-      this.props.listChannels()
-    })
-  }
-
-  render() {
-    let interactionsFinishedMarkup = <LoadingIndicator/>
-
-    if (this.state.interactionsFinished) {
-      interactionsFinishedMarkup = <Text>Lade Channels</Text>
+            this.props.listChannels()
+        })
     }
 
-    if (this.props.channels) {
-        interactionsFinishedMarkup = Object.keys(this.props.channels).map(
-            (id) => (<Text style={styles.listitem} key={'channel_id:' + id}>{this.props.channels[id].name}</Text>)
+    render() {
+        let interactionsFinishedMarkup = <LoadingIndicator/>
+
+        if (this.state.interactionsFinished) {
+            interactionsFinishedMarkup = <Text>Lade Channels</Text>
+        }
+
+        const channels = this.props.channels
+
+        if (channels) {
+            interactionsFinishedMarkup = Object.keys(channels).map(
+                (id) => (
+                    <TouchableHighlight
+                        key={'channel_id:' + id}
+                        onPress={this.props.joinChannel.bind(this, channels[id].channel_url)}
+                        activeOpacity={0.6}
+                        underlayColor="transparent">
+                        <Text style={styles.listitem}>{channels[id].name}</Text>
+                    </TouchableHighlight>)
+            )
+        }
+
+        return (
+            <View style={styles.container}>
+                <ActionBar title="Available Channels"/>
+                <ScrollView showsVerticalScrollIndicator={false} keyboardShouldPersistTaps={true}>
+                    {interactionsFinishedMarkup}
+                </ScrollView>
+            </View>
         )
     }
-
-    return (
-      <View style={styles.container}>
-        <ActionBar title="Available Channels"/>
-        <ScrollView showsVerticalScrollIndicator={false} keyboardShouldPersistTaps={true}>
-          {interactionsFinishedMarkup}
-        </ScrollView>
-      </View>
-    )
-  }
 }
 
 const styles = {
-  container: {
-    flex: 1,
-    backgroundColor: '#f7f8fc'
-  },
-  listitem: {
-    fontSize: 25
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#f7f8fc'
+    },
+    listitem: {
+        fontSize: 25
+    }
 }
 
 
 export default connect(
-  (state) => ({
-      channels: state.channels.list
-  }),
-  (dispatch) => {
-    return {
-      listChannels: () => dispatch(listChannels())
+    (state) => ({
+        channels: state.channels.list
+    }),
+    (dispatch) => {
+        return {
+            listChannels: () => dispatch(listChannels()),
+            joinChannel: (url) => dispatch(joinChannel(url))
+        }
     }
-  }
 )(ChannelsPage)
